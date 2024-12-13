2f7b1d786a8a063f739d6f2d14a0cc0b92b17bb7
Devon Ceccacci <dceccacci@gmail.com>
2024-12-12 19:15:32 -0800

    Update __init__.py

--- OSUCS461/Classes/Database/__init__.py
+++ OSUCS461/Classes/Database/__init__.py
@@ -2,6 +2,17 @@ from OSUCS461.Config import MySQL as DatabaseConfig
 from OSUCS461.ThirdParty.MySQL import MySQL
 from OSUCS461.Models import UserCreate, UserPostCreate
 
+# ---- For testing ----
+import sys
+import sys
+
+# Function to log an error message to standard error
+def log_error(message):
+    print(f"ERROR: {message}", file=sys.stderr)
+
+log_error(os.environ.get("DB_HOST"))
+log_error(int(os.environ.get("DB_PORT")))
+# ---------------------
 
 DB = MySQL(**DatabaseConfig)
 
